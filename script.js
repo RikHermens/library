63a3c4ae2d5044ea84bdd234445d1954a0fc3b59
RikHermens <rik_hermens@hotmail.com>
2021-04-15 11:01:57 +0200

    boeken toevoegen

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+let i = 0;
+const container = document.querySelector(".container");
 
 //Krijg het niet voor elkaar om het vanuit object constructor naar array te krijgen.
 // function book() {
@@ -8,7 +10,7 @@ let myLibrary = [];
 //     this.read = bookRead();
 // }
 const addButton = document.getElementById('btn');
-addButton.addEventListener("click", addToLibrary)
+addButton.addEventListener("click", addToLibrary);
 
 function addToLibrary() {
     let book = {
@@ -19,12 +21,34 @@ function addToLibrary() {
     };
     myLibrary.push(book);
     document.querySelector("form").reset();
+    displayBooks();
 }
 
 function bookRead() {
     if (document.getElementById("read").checked) {
-        return true;
+        return "true";
     } else {
-        return false
+        return "false"
     }
+}
+
+//Functie voegt eerder toegevoegde boeken ook al toe
+// function displayBooks() {
+//     for (i = 0; i < myLibrary.length; i++) {
+//         //create div met mylibrary[i]
+//         let content = document.createElement('div');
+//         content.classList.add("content");
+//         content.textContent = `${myLibrary[i].title} + ${myLibrary[i].author} + ${myLibrary[i].pages} + ${myLibrary[i].read}`;
+//         container.appendChild(content);
+//     }
+// }
+
+//Werkt, maar wat als array veranderd?
+function displayBooks() {
+
+    let content = document.createElement('div');
+    content.classList.add("content");
+    content.textContent = `${myLibrary[i].title} + ${myLibrary[i].author} + ${myLibrary[i].pages} + ${myLibrary[i].read}`;
+    container.appendChild(content);
+    i += 1;
 }
